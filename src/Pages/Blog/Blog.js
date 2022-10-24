d76d19ed2251347cc7a61235b84ec207e5791bcc
Fahim-05 <fahim45147@gmail.com>
2022-10-25 00:10:10 +0600

    blog page done

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import Accordion from 'react-bootstrap/Accordion';
 
 const Blog = () => {
     return (
-        <div>
-            <h2>This is blog</h2>
+        <div className='w-50 m-auto mt-5 border border-success'>
+            <Accordion defaultActiveKey="0" flush>
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header>What is cors?</Accordion.Header>
+                    <Accordion.Body className='text-secondary'>
+                        <small>CORS is a node.js package for providing a Connect/Express middleware that can be used to enable CORS with various options.</small>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                    <Accordion.Header>Why are you using firebase? What other options do you have to implement authentication?</Accordion.Header>
+                    <Accordion.Body className='text-secondary'>
+                        <small>
+                            Firebase Authentication provides backend services, easy-to-use SDKs, and ready-made UI libraries to authenticate users to your app. It supports authentication using passwords, phone numbers, popular federated identity providers like Google, Facebook and Twitter, and more.
+                            <br></br>
+                            Other options are: <br></br>
+                            MongoDB.
+                            Oracle Database.
+                            Amazon Redshift.
+                            DataStax Enterprise.
+                        </small>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="2">
+                    <Accordion.Header>How does the private route work?</Accordion.Header>
+                    <Accordion.Body className='text-secondary'>
+                        <small>
+                            The react private route component renders child components if the user is logged in. If not logged in the user is redirected to the /login page with the return url passed in the location state property.
+                        </small>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="3">
+                    <Accordion.Header>What is Node? How does Node work?</Accordion.Header>
+                    <Accordion.Body className='text-secondary'>
+                        Node:
+                        <small> Node.js is an open-source, cross-platform, back-end JavaScript runtime environment that runs on a JavaScript Engine and executes JavaScript code outside a web browser, which was designed to build scalable network applications</small><br></br><br></br>
+                        How Node works:
+                        <small> Node allows developers to write JavaScript code that runs directly in a computer process itself instead of in a browser. Node can, therefore, be used to write server-side applications with access to the operating system, file system, and everything else required to build fully-functional applications.</small>
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion>
         </div>
     );
 };
--- src/Routes/Routes/Router.js
+++ src/Routes/Routes/Router.js
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../layouts/Main";
+import Blog from "../../Pages/Blog/Blog";
 import Courses from "../../Pages/Courses/Courses";
 import Home from "../../Pages/Home/Home";
 
@@ -15,6 +16,10 @@ export const router = createBrowserRouter([
             {
                 path: '/courses',
                 element: <Courses></Courses>
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>
             }
         ]
     }
