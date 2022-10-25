4860975eff493284160e15b9fc3c5a1b8499ef99
Fahim-05 <fahim45147@gmail.com>
2022-10-25 15:20:29 +0600

    individual courses added

--- src/Pages/IndividualCourse/IndividualCourse.js
+++ src/Pages/IndividualCourse/IndividualCourse.js
@@ -3,11 +3,13 @@ import { useLoaderData } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import LeftSideOptions from '../Shared/LeftSideOptions/LeftSideOptions';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Image, Row } from 'react-bootstrap';
+import { FaDollarSign, FaUser } from "react-icons/fa";
 
 const IndividualCourse = () => {
     const oneCourseCard = useLoaderData();
     const { id, about, course_name, image, instructor, obtain, price } = oneCourseCard;
+    console.log(oneCourseCard)
     return (
         <div>
 
@@ -21,11 +23,27 @@ const IndividualCourse = () => {
                         <Card style={{ width: 600 }}>
                             <Card.Img variant="top" height={350} src={image} />
                             <Card.Body>
-                                <Card.Title >{course_name}</Card.Title>
+                                <Card.Title className='text-center fw-bold fs-4 text-success'>{course_name}</Card.Title>
+
+                                <div className='d-flex justify-content-between align-items-center'>
+                                    <div className='d-flex align-items-center mt-2'>
+                                        <Image
+                                            width="70px"
+                                            height="70px"
+                                            roundedCircle
+                                            src={instructor[0].img} />
+                                        <p className='ms-2 mt-1 fw-bold'>{instructor[0].name}</p>
+                                    </div>
+                                    <div className='d-flex align-items-center gap-1'>
+                                        <p className='me-5 text-danger fw-bold'><FaDollarSign className='mb-1'></FaDollarSign>{price}</p>
+                                        <p className='text-success fw-bold'><FaUser className='mb-1 me-1'></FaUser>{obtain}</p>
+                                    </div>
+                                </div>
+
                                 <Card.Text className='text-secondary'>
                                     {about}
                                 </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
+                                <Button variant="success">Go All Courses</Button>
                             </Card.Body>
                         </Card>
                     </Col>
