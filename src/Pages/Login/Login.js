3ab7115625f66ba2b66084dd99efbd7c0e44a14c
Fahim-05 <fahim45147@gmail.com>
2022-10-25 23:21:05 +0600

    password error done

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,33 +1,58 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import { FaGithub, FaGoogle } from "react-icons/fa";
 import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 import { GoogleAuthProvider } from 'firebase/auth';
 
+
 const Login = () => {
 
-    const {providerLogin }= useContext(AuthContext);
+    const [error, setError] = useState('');
 
+    const navigate = useNavigate();
+    const {providerLogin, signIn }= useContext(AuthContext);
 
     // google provider
     const googleProvider = new GoogleAuthProvider()
     const handleGoogleSignIn = () =>  {
         providerLogin(googleProvider)
-        .thee(result => {
+        .then(result => {
             const user = result.user;
             console.log(user);
+            navigate('/');
         })
         .catch(error => console.error(error));
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        signIn(email, password )
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+            form.reset();
+            setError('');
+            navigate('/');
+        })
+        .catch(error => {
+            console.error(error)
+            setError(error.message);
+        });
     }
 
 
 
     return (
         <div className='w-50 m-auto bg-secondary p-5 mt-1 rounded'>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-1 text-white">
                     <Form.Label >Email address</Form.Label>
                     <Form.Control name='email' type="email" placeholder="Enter email" required />
@@ -51,8 +76,8 @@ const Login = () => {
                 <Button variant="outline-warning" type="submit">
                     Login
                 </Button>
-                <Form.Text className="text-danger bg-dark">
-                    We'll never share your email with anyone else.
+                <Form.Text className="text-white bg-danger ms-2 rounded">
+                    {error}
                 </Form.Text>
             </Form>
         </div>
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
 const Register = () => {
+    const {createUser} = useContext(AuthContext);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(name, email, password);
+
+        createUser(email, password)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+            form.reset();
+        })
+        .catch(error=> console.error(error));
+
+    }
+
     return (
         <div className='w-50 m-auto bg-secondary p-5 mt-1 rounded'>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-1 text-white">
                     <Form.Label >Your name</Form.Label>
                     <Form.Control name='name' type="text" placeholder="your name" />
